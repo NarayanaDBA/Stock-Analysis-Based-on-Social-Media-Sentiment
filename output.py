c856a4a812ab66782fbb45b239bbf5e16f9a28a3
NarayanaDBA <pnarayanadba@gmail.com>
2024-12-06 22:37:21 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,47 @@
+## I got this Following Output as per the Twitter,py.    
+## OUTPUT
+RT @profits_trade: #BREAKOUTSTOCKS #BreakoutStock #stockstowatch #StocksInFocus #StocksToTrade #trading #StocksToBuy #stocktotrackList #Sto…
+Sentiment Analysis: Sentiment(polarity=0.0, subjectivity=0.0)
+Neutral
+RT @TradingMarvel: Pls take TTM seriously #Heg 
+#Graphite stocks on rock
+Sentiment Analysis: Sentiment(polarity=-0.3333333333333333, subjectivity=0.6666666666666666)
+Negative
+⚡️ A powerful surge of energy! ⚡️  
+
+@wangshu91548744 @Sie_eps @claragraciaf @NikhatNaazKhan1 @moh_heg @MiguelMercadoF
+zjume
+https://t.co/OYzCYBsJQK
+Sentiment Analysis: Sentiment(polarity=0.375, subjectivity=1.0)
+Positive
+#HEG and #Graphite India may come down sharply
+
+It is all pumped by cnbc and other tv anchors and lapped up by all paid stock tip sellers
+
+Retailers get trapped 
+
+There is no news from China on Graphite Electrodes export ban.
+Sentiment Analysis: Sentiment(polarity=-0.1513888888888889, subjectivity=0.35347222222222224)
+Negative
+RT @Nigel__DSouza: News 👉 China has imposed quota on flaky graphite  &amp; synthetic graphite exports 
+
+Reaction 👉 HEG &amp; Graphite India +25%-30…
+Sentiment Analysis: Sentiment(polarity=0.0, subjectivity=0.0)
+Neutral
+🌟 The feeling of something big is in the air! 🌠  
+
+https://t.co/BthBVXUUAw
+
+@vojta_klicka @Heg_Thingies @token_wire_ @DanWill1970 @PrinceP94635474 @slickvikk
+avofz
+Sentiment Analysis: Sentiment(polarity=0.0, subjectivity=0.1)
+Neutral
+RT @TrendTrader85: Just imagine people started tracking #HEG yesterday and today after breakout but we at 438 and that proper learning so g…
+Sentiment Analysis: Sentiment(polarity=0.0, subjectivity=0.1)
+Neutral
+@Pro1stAmendment @KevinRobertsTX @seanmdav @PeteHegseth Ultimately FBI will interview every one. Trump will prefer a clean nominee - no one wants a guy w a drinker’s reputation at DOD &amp; Trump is not a drinker himself. Heg’s own mother put a lot out &amp; then confused it all. Bet here is that Heg will withdraw.
+Sentiment Analysis: Sentiment(polarity=0.1533333333333333, subjectivity=0.7)
+Positive
+Stand with Heg. https://t.co/gnCHL6esE1
+Sentiment Analysis: Sentiment(polarity=0.0, subjectivity=0.0)
+Neutral
